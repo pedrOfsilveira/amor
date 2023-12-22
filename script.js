62dd4175e4429fd142b2657b9a8f2bdd0db0e504
pedrOfsilveira <pedrofsilveira24@gmail.com>
2023-12-22 03:36:26 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-function getLog(base, log) {
-  return Math.log(log) / Math.log(base);
+const botaoEsquerdo = document.querySelector('.botao-esquerda');
+const botaoDireito = document.querySelector('.botao-direita');
+
+let indexAtivo = 0;
+
+const grupos = document.getElementsByClassName('card-wrapper');
+
+botaoEsquerdo.addEventListener('click', () => {
+  botaoEsquerdo.classList.add('jello');
+
+  moveUltima();
+
+  setTimeout(() => {
+    botaoEsquerdo.classList.remove('jello');
+  }, 600);
+
+  setTimeout(() => {
+    botaoEsquerdo.removeAttribute('disabled');
+  }, 2000);
+});
+botaoDireito.addEventListener('click', () => {
+  botaoDireito.classList.add('jello');
+
+  botaoDireito.setAttribute('disabled', '');
+
+  moveProxima();
+
+  setTimeout(() => {
+    botaoDireito.classList.remove('jello');
+  }, 600);
+  setTimeout(() => {
+    botaoDireito.removeAttribute('disabled');
+  }, 2000);
+});
+
+function moveUltima() {}
+
+function moveProxima() {
+  const proximoIndex = indexAtivo + 1 <= grupos.length - 1 ? indexAtivo + 1 : 0;
+
+  const grupoAtual = document.querySelector(`[data-index="${indexAtivo}"]`),
+    proximoGrupo = document.querySelector(`[data-index="${proximoIndex}"]`);
+  console.log(grupoAtual);
+  grupoAtual.dataset.status = 'after';
+  proximoGrupo.dataset.status = 'active';
+
+  indexAtivo = proximoIndex;
 }
